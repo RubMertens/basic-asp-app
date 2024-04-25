d1772a620a1d5542dca11322934716047ed482ca
Ruben Mertens <ruben.mertens@infosupport.com>
2024-04-25 15:21:38 +0200

    Remove some dead code

--- Questioning.IntegrationTests/ExamApiTests.cs
+++ Questioning.IntegrationTests/ExamApiTests.cs
@@ -33,38 +33,6 @@ public class ExamApiTests
             "http://localhost/Exam/details/1"
         );
     }
-
-    public static async Task<IHtmlDocument> GetDocumentAsync(
-        HttpResponseMessage response)
-    {
-        var content = await response.Content.ReadAsStringAsync();
-        var document = await BrowsingContext.New()
-            .OpenAsync(ResponseFactory, CancellationToken.None);
-        return (IHtmlDocument)document;
-
-        void ResponseFactory(VirtualResponse htmlResponse)
-        {
-            htmlResponse
-                .Address(response.RequestMessage.RequestUri)
-                .Status(response.StatusCode);
-
-            MapHeaders(response.Headers);
-            MapHeaders(response.Content.Headers);
-
-            htmlResponse.Content(content);
-
-            void MapHeaders(HttpHeaders headers)
-            {
-                foreach (var header in headers)
-                {
-                    foreach (var value in header.Value)
-                    {
-                        htmlResponse.Header(header.Key, value);
-                    }
-                }
-            }
-        }
-    }
 }
 
 public class CustomWebApplicationFactory : WebApplicationFactory<Program>
@@ -76,5 +44,7 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
         Environment.SetEnvironmentVariable(
             "ConnectionStrings:DefaultConnection",
             "Data Source=file::memory:;Cache=Shared");
+        
+        
     }
 }
